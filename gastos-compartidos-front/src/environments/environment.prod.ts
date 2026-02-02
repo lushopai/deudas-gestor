9635a02456ae97fb3d14691871e8bbee83ae8417
Luis <luxo.lyr@gmail.com>
2026-02-02 15:55:48 -0300

    chore: Update docker-compose for external DB and Google secrets

--- gastos-compartidos-front/src/environments/environment.prod.ts
+++ gastos-compartidos-front/src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.tudominio.com/api',
+  get apiUrl() {
+    const hostname = window.location.hostname;
+    // En producción (Docker), el backend estará en el puerto 9150 del mismo host
+    return `http://${hostname}:9150/api`;
+  },
   google: {
     clientId: '412131551809-ipbsdonp0927n7dgq6vnut05d67posda.apps.googleusercontent.com'
   }
