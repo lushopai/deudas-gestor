4fda1a0415e13cf32b083b32864d8e0449bfd692
Luis Yanez <luxo.lyr@gmail.com>
2026-02-16 12:00:59 -0300

    reorganizar importaciones en la clase Usuario para mejorar la legibilidad

--- gastos-compartidos/src/main/java/com/gastos/gastos_compartidos/entity/Usuario.java
+++ gastos-compartidos/src/main/java/com/gastos/gastos_compartidos/entity/Usuario.java
@@ -1,13 +1,27 @@
 package com.gastos.gastos_compartidos.entity;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
